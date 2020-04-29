646f4abf9acb132769b7daaec2d09ab36cb6edc1
Jared <janstett9@gmail.com>
2020-04-29 02:29:27 -0400

    Project Solution Code

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'fsnd-ja.auth0.com', // the auth0 domain prefix
+    url: 'fsnd-ja', // the auth0 domain prefix
     audience: 'fullstack-cafe', // the audience set for the auth0 app
     clientId: 'lxnDMeo2N47ctiY1JB6DPyPuMJDHs5BX', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
